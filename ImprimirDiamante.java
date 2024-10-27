e56efdcc57d029e364de7f9d87b3f4667822f63f
Marlon Xavier Delgado Ruiz <marlonxavi6@gmail.com>
2024-10-27 13:17:12 -0500

    Ejercicio 3

---
+++ ImprimirDiamante.java
@@ -0,0 +1,13 @@
+public class ImprimirDiamante {
+    public static void main(String[] args) {
+        System.out.printf("%5s*%n", "");
+        System.out.printf("%4s***%n", "");
+        System.out.printf("%3s*****%n", "");
+        System.out.printf("%2s*******%n", "");
+        System.out.printf("%1s*********%n", "");
+        System.out.printf("%2s*******%n", "");
+        System.out.printf("%3s*****%n", "");
+        System.out.printf("%4s***%n", "");
+        System.out.printf("%5s*%n", "");
+    }
+}
